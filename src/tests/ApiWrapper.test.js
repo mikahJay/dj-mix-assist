624a2c90a384a6bf449034b546a1862a8bc15a7a
mikah-village <mtrent@villagemd.com>
2025-12-19 11:54:09 -0600

    got to 100% coverage on ApiWrapper

--- src/tests/ApiWrapper.test.js
+++ src/tests/ApiWrapper.test.js
@@ -1,4 +1,10 @@
+import { waitFor } from '@testing-library/react';
 import ApiWrapper from '../ApiWrapper';
+import '@testing-library/jest-dom';
+
+afterEach(() => {
+  jest.restoreAllMocks();
+});
 
 it('httpRequestWrapper returns null when no auth provided', async () => {
   const apiWrapper = new ApiWrapper();
@@ -10,4 +16,26 @@ it('getAccessToken returns null when no auth provided', async () => {
   const apiWrapper = new ApiWrapper();
   const ret = await apiWrapper.getAccessToken(null, null, null);
   expect(ret).toBeNull();
+});
+
+it('httpRequestWrapper calls correct \'fetch\' and returns the data', async () => {
+  jest.spyOn(global, 'fetch').mockResolvedValue({
+    json: jest.fn().mockResolvedValue({"data": "hello world"}),
+  });
+  const apiWrapper = new ApiWrapper();
+  const ret = await apiWrapper.httpRequestWrapper('na', 'na', null, null);
+  await waitFor(() => {
+    expect(ret).toStrictEqual({data: 'hello world'});
+  });
+});
+
+it('getAccessToken calls correct \'fetch\' and returns only the access token', async () => {
+  jest.spyOn(global, 'fetch').mockResolvedValue({
+    json: jest.fn().mockResolvedValue({"access_token": "access_token1234"}),
+  });
+  const apiWrapper = new ApiWrapper();
+  const ret = await apiWrapper.getAccessToken('na', 'na', 'na');
+  await waitFor(() => {
+    expect(ret).toBe("access_token1234");
+  });
 });
